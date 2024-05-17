856a7521d5266757217aa1d195f7c4e7b00ac8f6
Kota Tsuyuzaki <bloodeagle40234@gmail.com>
2024-05-17 21:57:23 +0900

    Add Initial Files

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # dp-trtllm
-Dummy Plug for TensorRT-LLM used by transformers pipeline
+Dummy Plug for NVIDIA TensorRT-LLM used by transformers pipeline.
+
+## Installation
+```
+git clone https://github.com/bloodeagle40234/dp-trtllm
+cd dp-trtllm
+pip install .
+```
+
+## Usage
+Create DummyPlug instance via TensorRT-LLM engine directory, then
+pass the instance to transformers pipeline
+
+```
+from transformers import AutoTokenizer
+from transformers.pipelines import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained(
+    "<Tokenizer Path of base model>", device_map="cuda")
+model = DummyPlug("<TensorRT-LLM engine path>")
+
+pipe = pipeline(
+    "text-generation",
+    model=model,
+    framework="pt",
+    tokenizer=tokenizer,
+    eos_token=tokenizer.eos_token_id,
+    pad_token=tokenizer.pad_token_id,
+    max_new_tokens=1024,
+    device_map="cuda"
+)
+```
+
+## References
+- [NVIDIA TensorRT-LLM](https://github.com/NVIDIA/TensorRT-LLM)
+- [transformers](https://github.com/huggingface/transformers/tree/main)
---
+++ dp_trtllm/__init__.py
@@ -0,0 +1,59 @@
+# Copyright 2024 Kota Tsuyuzaki
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import torch
+import tensorrt_llm
+from tensorrt_llm.runtime import ModelRunner
+from transformers.modeling_utils import PreTrainedModel
+from transformers import PretrainedConfig
+
+
+class DummyPlug(PreTrainedModel):
+    """
+    This DummpyPlug class is intended to be used as Pre-Trained model
+    for transformers pipeline.
+    """
+    def __init__(self, engine_path):
+        self.engine_path = engine_path
+        self.runner = self._get_trt_instance()
+        self.config = PretrainedConfig.from_dict(
+            self.runner.session._model_config.__dict__)
+
+    @property
+    def model_id(self):
+        return self.engine_path
+
+    @property
+    def _modules(self):
+        return {}
+
+    def generate(self, **kwargs):
+        batch_input_ids = kwargs.pop("input_ids")
+        kwargs["end_id"] = kwargs.pop("eos_token")
+        kwargs["pad_id"] = kwargs.pop("pad_token")
+        result = self.runner.generate(batch_input_ids, **kwargs)
+        return result[0]
+
+    def to(self, device):
+        # tensorrt-llm only works on CUDA so this api
+        # (like as model.to("cuda")) should be just mock.
+        pass
+
+    def _get_trt_instance(self):
+        tensorrt_llm.logger.set_level("info")
+        runtime_rank = tensorrt_llm.mpi_rank()
+        torch.cuda.set_device(0)
+        runner = ModelRunner.from_dir(
+            engine_dir=self.engine_path, rank=runtime_rank)
+        return runner
